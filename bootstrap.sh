45a93751a9ea46fec4ecfb42a8c630a7790c18e2
Pravinchandar Raajendiran <pravinchandar@gmail.com>
2017-07-06 23:25:25 +1000

    Added bootstrap.sh file to install packages Configurator depends on

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Following are the python packages that Configurator will depend on
+# It is essential that these are installed before running configurator.
+apt-get install python-apt python-yaml -y
